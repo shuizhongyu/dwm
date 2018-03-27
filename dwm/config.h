018cebcdb50a3102c9080d879b0c5ecc90eee424
ljx <jiaxin10@mail.ustc.edu.cn>
2018-03-27 22:17:26 +0800

    update README and edit config.h

--- dwm/config.h
+++ dwm/config.h
@@ -70,7 +70,7 @@ static const Rule rules[] = {
 };
 
 /* layout(s) */
-static const float mfact      = 0.50; /* factor of master area size [0.05..0.95] */
+static const float mfact      = 0.55; /* factor of master area size [0.05..0.95] */
 static const int nmaster      = 1;    /* number of clients in master area */
 static const Bool resizehints = False; /*True;*/ /* True means respect size hints in tiled resizals */
 
